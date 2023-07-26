660d22e061782d85551fc50c084576025bfaf94d
MyBooty165 <126267638+MyBooty165@users.noreply.github.com>
2023-07-26 13:20:02 -0500

    Update antimatter.url.js

--- antimatter/antimatter.url.js
+++ antimatter/antimatter.url.js
@@ -1,30 +1,3 @@
-const initiateButton = document.querySelector('#initiate');
-const urlInput = document.querySelector('#url-input');
-const errorDisc = document.getElementById("error-disc");
-
-initiateButton.addEventListener('click', initiate);
-urlInput.addEventListener('keyup', handleKeyUp);
-
-function initiate() {
-  const url = urlInput.value.trim();
-  if (url !== "") {
-    redirectToGateway(url);
-  } else {
-    showError();
-  }
-}
-
-function handleKeyUp(e) {
-  if (e.key === "Enter") {
-    initiate();
-  }
-}
-
-function redirectToGateway(url) {
-  const gatewayUrl = "/search/query/?url=" + encodeURIComponent(url);
-  location.href = gatewayUrl;
-}
-
-function showError() {
-  errorDisc.style.display = "inherit";
-}
+const initiateButton = document.querySelector('#initiate'); const urlInput = document.querySelector('#url-input'); const errorDisc = document.getElementById("error-disc");
+initiateButton.addEventListener('click', () => { const url = urlInput.value.trim(); if (url !== "") { const gatewayUrl = `/search/query/?url=${encodeURIComponent(url)}`; location.href = gatewayUrl; } else { errorDisc.textContent = "URL cannot be empty."; errorDisc.style.display = "inherit"; } });
+urlInput.addEventListener('keyup', (e) => { if (e.key === "Enter") { const url = urlInput.value.trim(); if (url !== "") { const gatewayUrl = `/search/query/?url=${encodeURIComponent(url)}`; location.href = gatewayUrl; } else { errorDisc.textContent = "URL cannot be empty."; errorDisc.style.display = "inherit"; } } });
