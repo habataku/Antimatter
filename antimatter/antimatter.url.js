730248ada5f7d5d614780df6f22641c28100c6da
MyBooty165 <126267638+MyBooty165@users.noreply.github.com>
2023-07-26 13:31:42 -0500

    Update antimatter.url.js

--- antimatter/antimatter.url.js
+++ antimatter/antimatter.url.js
@@ -1,3 +1,33 @@
-const initiateButton = document.querySelector('#initiate'); const urlInput = document.querySelector('#url-input'); const errorDisc = document.getElementById("error-disc");
-initiateButton.addEventListener('click', () => { const url = urlInput.value.trim(); if (url !== "") { const gatewayUrl = `/search/query/?url=${encodeURIComponent(url)}`; location.href = gatewayUrl; } else { errorDisc.textContent = "URL cannot be empty."; errorDisc.style.display = "inherit"; } });
-urlInput.addEventListener('keyup', (e) => { if (e.key === "Enter") { const url = urlInput.value.trim(); if (url !== "") { const gatewayUrl = `/search/query/?url=${encodeURIComponent(url)}`; location.href = gatewayUrl; } else { errorDisc.textContent = "URL cannot be empty."; errorDisc.style.display = "inherit"; } } });
+const initiateButton = document.querySelector('#initiate');
+const urlInput = document.querySelector('#url-input');
+const errorDisc = document.getElementById('error-disc');
+
+initiateButton.addEventListener('click', handleButtonClick);
+urlInput.addEventListener('keyup', handleInputKeyUp);
+
+function handleButtonClick() {
+  const url = urlInput.value.trim();
+  if (url !== '') {
+    const gatewayUrl = `/search/query/?url=${encodeURIComponent(url)}`;
+    location.href = gatewayUrl;
+  } else {
+    showError('URL cannot be empty.');
+  }
+}
+
+function handleInputKeyUp(e) {
+  if (e.key === 'Enter') {
+    const url = urlInput.value.trim();
+    if (url !== '') {
+      const gatewayUrl = `/search/query/?url=${encodeURIComponent(url)}`;
+      location.href = gatewayUrl;
+    } else {
+      showError('URL cannot be empty.');
+    }
+  }
+}
+
+function showError(message) {
+  errorDisc.textContent = message;
+  errorDisc.style.display = 'inherit';
+}
